3c00cc35ab2fdf52970e60c479b967531bf2b095
MeH2607 <moha898h@stud.kea.dk>
2023-10-05 11:32:00 +0200

    Startet på reservations

---
+++ pages/reservation/reservation.js
@@ -0,0 +1,25 @@
+import { API_URL } from "../../settings.js";
+import {
+  sanitizeStringWithTableRows,
+  handleHttpErrors,
+  makeOptions,
+} from "../../utils.js";
+const URL = API_URL + "/reservations";
+
+export async function initAllReservations() {
+    document.getElementById("error").innerText = ""
+try{
+    const reservations = await fetch(URL, makeOptions("GET", null, false/*TODO ændrer til true senere*/)).then(handleHttpErrors);
+    const rows = reservations.map(res =  `
+        <tr>
+      <td>${res.id}</td>
+      <td>${res.rentalDate}</td>
+      <td>${res.username}</td>
+      <td>${res.reservationDate}</td>
+      <td>${res.activities.join(",")}</td>
+    </tr>
+        `
+    ).join("\n")
+}
+
+}
