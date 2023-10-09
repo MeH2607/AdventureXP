7a1a6cc8dfef5e2f582f729b147d7e121a76d9b7
OsamaMansour25 <osam0289@stud.kea.dk>
2023-10-09 14:37:02 +0200

    Endpoint to retrieve reservations for specific user, logged in user and activity done

--- src/main/java/dat3/adventureXP/api/ReservationController.java
+++ src/main/java/dat3/adventureXP/api/ReservationController.java
@@ -2,11 +2,9 @@ package dat3.adventureXP.api;
 
 import dat3.adventureXP.dto.ReservationResponse;
 import dat3.adventureXP.service.ReservationService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -22,6 +20,21 @@ public class ReservationController {
     @GetMapping
     public List<ReservationResponse> getReservations() {
         List<ReservationResponse> res = reservationService.getReservations();
+        return res;
+    }
+    @GetMapping("/{activityName}")
+    public List<ReservationResponse> getReservationsForSpecificActivity(@PathVariable String activityName) {
+        List<ReservationResponse> res = reservationService.getReservationsForActivity(activityName);
+        return res;
+    }
+    @GetMapping("/reservations-for-authenticated")
+    public List<ReservationResponse> getReservationsForUser(Principal principal){
+        List<ReservationResponse> res = reservationService.getReservationsMadeByUser(principal.getName());
+        return res;
+    }
+    @GetMapping("user/{userName}")
+    public List<ReservationResponse> getReservationsForUser(@PathVariable String userName){
+        List<ReservationResponse> res = reservationService.getReservationsMadeByUser(userName);
         return res;
     }
 
