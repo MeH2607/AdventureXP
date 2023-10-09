6d5c1d430298d1dc1ef4fd87396448331fc4ec7c
OsamaMansour25 <osam0289@stud.kea.dk>
2023-10-09 16:27:39 +0200

    Git merge removed constructor for ReservationService. ADDED again.

--- src/main/java/dat3/adventureXP/service/ReservationService.java
+++ src/main/java/dat3/adventureXP/service/ReservationService.java
@@ -85,24 +85,4 @@ public class ReservationService {
     }
 
 
-    public ReservationResponse makeReservations(ReservationRequest body) {
-        Reservation reservation = new Reservation();
-
-        //Sets user who made the reservation
-        reservation.setUser(userWithRolesRepository.findByUsername(body.getUsername()));
-        //Sets rental date
-        reservation.setRentalDate(body.getRentalDate());
-        //Sets activities
-        for(String activityName : body.getActivities()){
-            System.out.println("Before repository" + activityName);
-            Activity newActivity = activityRepository.findByName(activityName);
-            System.out.println("After repository" + newActivity.getName());
-            reservation.addActivity(newActivity);
-        }
-        //Saves reservation in database
-        reservationRepository.save(reservation);
-
-        //Creates and returns new ReservationResponse
-        return new ReservationResponse(reservation);
-    }
 }
