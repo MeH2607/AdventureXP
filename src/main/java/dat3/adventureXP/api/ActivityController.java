eae43c4576c747f687528ad11f7d79db2880d946
mathiasfri <55091986+mathiasfri@users.noreply.github.com>
2023-10-11 11:49:43 +0200

    added method security to some endpoints

--- src/main/java/dat3/adventureXP/api/ActivityController.java
+++ src/main/java/dat3/adventureXP/api/ActivityController.java
@@ -2,10 +2,8 @@ package dat3.adventureXP.api;
 
 import dat3.adventureXP.dto.ActivityResponse;
 import dat3.adventureXP.service.ActivityService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,11 +16,14 @@ public class ActivityController {
     public ActivityController(ActivityService activityService) {
         this.activityService = activityService;
     }
+
     @GetMapping
     List<ActivityResponse> getActivities() {
         return activityService.getActivities();
     }
-    
+
+    // ADMIN
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PutMapping
     ActivityResponse updateActivity(@RequestBody ActivityResponse activityResponse) {
         return activityService.updateActivity(activityResponse);
--- src/main/java/dat3/adventureXP/api/ReservationController.java
+++ src/main/java/dat3/adventureXP/api/ReservationController.java
@@ -3,6 +3,7 @@ package dat3.adventureXP.api;
 import dat3.adventureXP.dto.ReservationRequest;
 import dat3.adventureXP.dto.ReservationResponse;
 import dat3.adventureXP.service.ReservationService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -18,21 +19,20 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
+    // ADMIN
     @GetMapping
     public List<ReservationResponse> getReservations() {
         List<ReservationResponse> res = reservationService.getReservations();
         return res;
     }
+
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('EMPLOYEE')")
     @GetMapping("/{activityName}")
     public List<ReservationResponse> getReservationsForSpecificActivity(@PathVariable String activityName) {
         List<ReservationResponse> res = reservationService.getReservationsForActivity(activityName);
         return res;
     }
-    @GetMapping("/reservations-for-authenticated")
-    public List<ReservationResponse> getReservationsForUser(Principal principal){
-        List<ReservationResponse> res = reservationService.getReservationsMadeByUser(principal.getName());
-        return res;
-    }
+
     @GetMapping("user/{userName}")
     public List<ReservationResponse> getReservationsForUser(@PathVariable String userName){
         List<ReservationResponse> res = reservationService.getReservationsMadeByUser(userName);
