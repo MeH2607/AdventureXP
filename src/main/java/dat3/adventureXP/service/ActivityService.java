91e43ca449ece692e1a0828a8313c70a37299a64
MeH2607 <moha898h@stud.kea.dk>
2023-10-12 09:13:24 +0200

    tilføjet userRespository

--- src/main/java/dat3/adventureXP/service/ActivityService.java
+++ src/main/java/dat3/adventureXP/service/ActivityService.java
@@ -2,6 +2,7 @@ package dat3.adventureXP.service;
 
 import dat3.adventureXP.dto.ActivityResponse;
 import dat3.adventureXP.entity.Activity;
+import dat3.adventureXP.entity.User;
 import dat3.adventureXP.repository.ActivityRepository;
 import dat3.adventureXP.repository.UserRepository;
 import dat3.security.entity.UserWithRoles;
@@ -13,9 +14,11 @@ import java.util.stream.Collectors;
 @Service
 public class ActivityService {
     ActivityRepository activityRepository;
+    UserRepository userRepository;
 
-    public ActivityService(ActivityRepository activityRepository) {
+    public ActivityService(ActivityRepository activityRepository, UserRepository userRepository) {
         this.activityRepository = activityRepository;
+        this.userRepository = userRepository;
     }
     public List<ActivityResponse> getActivities() {
          List<Activity> activities = activityRepository.findAll();
