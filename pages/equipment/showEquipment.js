450b5768660a52541fb789b241a04bc22d3b4775
mathiasfri <55091986+mathiasfri@users.noreply.github.com>
2023-10-12 13:48:06 +0200

    status can now be changed

--- pages/equipment/showEquipment.js
+++ pages/equipment/showEquipment.js
@@ -4,26 +4,49 @@ import { sanitizeStringWithTableRows, handleHttpErrors, makeOptions } from "../.
 const URL = API_URL + "/equipment";
 
 export async function initEquipment() {
-    /*const options = {}
-    options.method = "GET";
-    options.headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": "Bearer " + localStorage.getItem("token")
-    }*/
-
     const options = makeOptions("GET", null, true);
     const equipment = await fetch(URL, options).then(res => res.json());
+  
+    const tableRows = document.getElementById("table-rows");
+    tableRows.innerHTML = "";
 
-    const equipmentTableRows = equipment.map(equipment => `
-        <tr>
-            <td>${equipment.name}</td>
-            <td>${equipment.status}</td>
-            <td>${equipment.activity}</td>
-        </tr>
-    `)
+  
+    equipment.forEach(equipmentItem => {
+      const row = document.createElement("tr");
 
-    const tableRowsAsStr = equipmentTableRows.join("")
-
-    document.getElementById("table-rows").innerHTML = sanitizeStringWithTableRows(tableRowsAsStr)
-}
+      row.innerHTML = `
+        <td>${equipmentItem.name}</td>
+        <td>
+          <select class="status-dropdown">
+            <option value="Available" ${equipmentItem.status === "Available" ? "selected" : ""}>Available</option>
+            <option value="Unavailable" ${equipmentItem.status === "Unavailable" ? "selected" : ""}>Unavailable</option>
+          </select>
+        </td>
+        <td>${equipmentItem.activity}</td>
+      `;
+      row.setAttribute("data-id", equipmentItem.name);
+      tableRows.appendChild(row);
+    });
+  
+    
+    tableRows.addEventListener("change", async (event) => {
+      if (event.target.classList.contains("status-dropdown")) {
+        const selectedStatus = event.target.value;
+        const row = event.target.closest("tr");
+        const itemId = row.getAttribute("data-id");
+      
+        const itemToUpdate = equipment.find(item => item.id === itemId);
+        if (itemToUpdate) {
+          itemToUpdate.status = (selectedStatus === "Available") ? "Available" : "Unavailable";
+        }
+  
+        const updateOptions = makeOptions("PUT", { status: selectedStatus }, true);
+        try {
+            await fetch(`${URL}/${itemId}`, updateOptions).then(handleHttpErrors);
+        }
+        catch(error) {
+            alert(error);
+        }
+      }
+    });
+  }
